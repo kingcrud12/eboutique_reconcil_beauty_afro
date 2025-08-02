3410f0183fc83939b041a5e604dcf54b41ac2a1a
kingcrud12 <dipitay@gmail.com>
2025-08-02 10:12:49 +0200

    update

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -25,7 +25,7 @@ import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handleba
         from: '"Mon Eshop" <dipitay@gmail.com>',
       },
       template: {
-        dir: join(process.cwd(), 'dist', 'templates'),
+        dir: join(__dirname, 'templates'),
         adapter: new HandlebarsAdapter(),
         options: {
           strict: true,
