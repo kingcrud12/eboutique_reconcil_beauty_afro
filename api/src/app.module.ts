bb0af5afcd0dec0c5c749705eba13baef253ee7c
kingcrud12 <dipitay@gmail.com>
2025-08-02 10:29:23 +0200

    updating template issue

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -25,7 +25,7 @@ import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handleba
         from: '"Mon Eshop" <dipitay@gmail.com>',
       },
       template: {
-        dir: join(__dirname, 'templates'),
+        dir: join(__dirname, '..', 'src', 'templates'),
         adapter: new HandlebarsAdapter(),
         options: {
           strict: true,
