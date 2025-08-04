6eae4a85c1070531495785850311b23bb8ee2ce2
kingcrud12 <dipitay@gmail.com>
2025-08-04 11:51:47 +0200

    update mailer title

--- api/prisma/migrations/20250722230216_update/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `rôle` on the `User` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `User` DROP COLUMN `rôle`,
-    ADD COLUMN `role` ENUM('utilisateur', 'admin') NOT NULL DEFAULT 'utilisateur';
