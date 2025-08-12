077b0bafe2591a175c6cc7b5e868380b74eeaa2f
kingcrud12 <dipitay@gmail.com>
2025-08-12 02:57:59 +0200

    adding confirmation mail

--- api/src/modules/payments/payments.module.ts
+++ api/src/modules/payments/payments.module.ts
@@ -5,8 +5,10 @@ import { StripeProvider } from './stripe.provider';
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
 import { StripeWebhookController } from './stripe.webhook.controller';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
+  imports: [MailerModule],
   providers: [PrismaService, StripeProvider, PaymentService],
   exports: [PaymentService],
   controllers: [PaymentController, StripeWebhookController],
