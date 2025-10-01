8d8810f43b9a2977dfc79ad1300119c5223c3b6b
kingcrud12 <dipitay@gmail.com>
2025-10-01 21:10:08 +0200

    updating logout logic

--- client/src/connect_to_api/authService.ts
+++ client/src/connect_to_api/authService.ts
@@ -16,24 +16,7 @@ export const login = async () => {
 
 export const logout = async () => {
   try {
-    // Appel à la déconnexion côté serveur, si nécessaire
     await api.post("/auth/logout");
-
-    // Déconnexion d'Auth0 (cela met fin à la session Auth0)
-    const auth0LogoutUrl = `https://${
-      process.env.REACT_APP_AUTH0_DOMAIN
-    }/v2/logout?client_id=${
-      process.env.REACT_APP_AUTH0_CLIENT_ID
-    }&returnTo=${encodeURIComponent(window.location.origin)}`;
-
-    // Suppression du token et du code verifier
-    sessionStorage.removeItem("auth_token"); // Supprimer le token
-    sessionStorage.removeItem("pkce_code_verifier"); // Supprimer le verifier
-
-    console.log("Redirecting to Auth0 logout URL:", auth0LogoutUrl);
-
-    // Redirection vers Auth0 pour se déconnecter proprement
-    window.location.href = auth0LogoutUrl; // Important : assurez-vous que ce redirige correctement
   } catch (err) {
     console.error("Erreur lors de la déconnexion", err);
   }
