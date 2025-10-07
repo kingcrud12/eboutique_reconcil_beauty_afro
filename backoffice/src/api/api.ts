9b91a9a145b98a2fa62d56b64edaa52773711189
kingcrud12 <dipitay@gmail.com>
2025-10-07 15:14:58 +0200

    updating bo authentication

--- backoffice/src/api/api.ts
+++ backoffice/src/api/api.ts
@@ -2,16 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
-  withCredentials: false,
-});
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("auth_token");
-  if (token) {
-    config.headers = config.headers ?? {};
-    config.headers["Authorization"] = `Bearer ${token}`;
-  }
-  return config;
+  withCredentials: true,
 });
 
 export default api;
