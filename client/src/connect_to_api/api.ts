eb191b15be96ab50a5dee98571ff08edfac9fb4b
kingcrud12 <dipitay@gmail.com>
2025-10-08 12:51:06 +0200

    contexts, authentication logic rollback

--- client/src/connect_to_api/api.ts
+++ client/src/connect_to_api/api.ts
@@ -1,17 +1,13 @@
-// src/connect_to_api/api.ts
 import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
-  withCredentials: false, // plus besoin si on envoie le JWT dans l'header
 });
 
-// Intercepteur pour ajouter le token dans les headers
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("auth_token");
+  const token = localStorage.getItem("token");
   if (token) {
-    config.headers = config.headers ?? {};
-    config.headers["Authorization"] = `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
